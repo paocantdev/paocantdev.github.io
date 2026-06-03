562b35c1e37ec871bf1d3fc2108ca3ac7751c4a4
Pawii <paolomurillo0109@gmail.com>
2026-06-03 23:03:53 +0800

    dark mode

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,38 @@
 import { Link } from 'react-router-dom'
-import { FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { FaGithub, FaLinkedin, FaFacebook, FaMoon, FaSun } from 'react-icons/fa'
 import logo from '../assets/logopic.png'
 import '../styles/navigation.css'
 
 export default function Navigation() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true'
+  })
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode')
+    } else {
+      document.body.classList.remove('dark-mode')
+    }
+    localStorage.setItem('darkMode', isDarkMode)
+  }, [isDarkMode])
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">
-          <img src={logo} alt="Paocantdev Logo" className="logo-image" />
-        </Link>
+        <div className="nav-logo-section">
+          <Link to="/" className="nav-logo">
+            <img src={logo} alt="Paocantdev Logo" className="logo-image" />
+          </Link>
+          <button className="theme-toggle" onClick={toggleDarkMode} title="Toggle dark mode">
+            {isDarkMode ? <FaSun /> : <FaMoon />}
+          </button>
+        </div>
         <ul className="nav-menu">
           <li className="nav-item">
             <Link to="/" className="nav-link">Home</Link>
